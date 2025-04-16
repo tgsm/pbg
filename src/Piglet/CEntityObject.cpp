f0522dee56df2eb06752f072f2d46209237a8ea5
tgsm <doodrabbit@hotmail.com>
2025-04-16 18:29:19 -0500

    Begin work on CEntityObject

---
+++ src/Piglet/CEntityObject.cpp
@@ -0,0 +1,107 @@
+#include "CEntityObject.h"
+#include "CEntityBhvTagPosition.h"
+#include "CEntityBhvTagOrientation.h"
+#include <cstring>
+
+CEntityObject::CEntityObject(CEntityManager* entity_manager, std::string name) : CEntity(entity_manager, name) {
+    m_unk24 = 0;
+    m_unk28 = 0;
+    m_unk2C = 0;
+
+    AddFlag(1 << 2);
+
+    m_unk30 = 3.4028235e38f;
+    m_unk34 = 3.4028235e38f;
+    m_unk38 = 3.4028235e38f;
+    m_unk3C = 3.4028235e38f;
+}
+
+// Incomplete
+CEntityObject::~CEntityObject() {
+    m_unk28 = 0;
+    if (m_unk2C != NULL) {
+        delete m_unk2C;
+    }
+    m_unk2C = NULL;
+    m_unk24 = 0;
+
+    if (m_unk2C != NULL) {
+        delete m_unk2C;
+    }
+}
+
+void CEntityObject::Reset() {
+    CEntity::Reset();
+}
+
+void CEntityObject::Update(f32 dt_maybe) {
+    CEntity::Update(dt_maybe);
+    if (IsFlagged(1 << 0) && IsFlagged(1 << 7) == TRUE) {
+        // FIXME: Fakematch
+        asm { cmpwi r3, 1 }
+    }
+}
+
+void CEntityObject::Init() {
+    CEntity::Init();
+    UpdateVolumes();
+}
+
+void CEntityObject::Render(f32 dt_maybe) {
+    if (!IsFlagged(1 << 8) && IsFlagged(1 << 1)) {
+        // FIXME: Fakematch
+        asm { cmpwi r3, 0 }
+    }
+}
+
+void CEntityObject::Parse(DkXmd::CChunkIterator iter) {
+    char buf[128];
+    DkXmd::CChunkIterator dest;
+
+    CEntity::Parse(iter);
+
+    if (iter.GetFirstChildChunk(dest) != TRUE) {
+        return;
+    }
+
+    while (TRUE) {
+        strcpy(buf, dest.GetName());
+
+        if (strcmp(buf, "Collisions") == 0) {
+            ParseCollisions(dest);
+        }
+
+        if (dest.GetNextSiblingChunk(dest) != TRUE) {
+            break;
+        }
+    }
+}
+
+void CEntityObject::ParseBehavior(DkXmd::CChunkIterator iter, CEntityBhvTagBehavior* behavior) {
+    char buf[128];
+    DkXmd::CChunkIterator dest;
+
+    CEntity::ParseBehavior(iter, behavior);
+
+    if (iter.GetFirstChildChunk(dest) != TRUE) {
+        return;
+    }
+
+    while (TRUE) {
+        strcpy(buf, dest.GetName());
+
+        if (strcmp(buf, "Position") == 0) {
+            CEntityBhvTagPosition* position = new CEntityBhvTagPosition;
+            position->Parse(dest);
+            behavior->AddData(position);
+        } else if (strcmp(buf, "Position") == 0) {
+            CEntityBhvTagOrientation* orientation = new CEntityBhvTagOrientation;
+            orientation->Parse(dest);
+            behavior->AddData(orientation);
+        }
+
+        if (dest.GetNextSiblingChunk(dest) != TRUE) {
+            break;
+        }
+    }
+}
---
+++ src/Piglet/CEntityObject.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "engine/CChunkIterator.h"
+#include "engine/DKW_V3d.h"
+#include "CEntity.h"
+
+// TODO
+class CDKW_Matrix {};
+
+class CEntityObject : public CEntity {
+private:
+    u32 m_unk24;
+    u32 m_unk28;
+    void* m_unk2C;
+    f32 m_unk30;
+    f32 m_unk34;
+    f32 m_unk38;
+    f32 m_unk3C;
+
+public:
+    CEntityObject(CEntityManager* entity_manager, std::string name);
+    virtual ~CEntityObject();
+
+    virtual void AddFlag(u32 flag);
+    virtual void DelFlag(u32 flag);
+    virtual u32 GetType();
+    virtual void Init();
+    virtual void Reset();
+    virtual void Update(f32 dt_maybe);
+    virtual void Render(f32 dt_maybe);
+    virtual void Parse(DkXmd::CChunkIterator iter);
+    virtual void ParseBehavior(DkXmd::CChunkIterator iter, CEntityBhvTagBehavior* behavior);
+    virtual u32 GetSaveSize();
+    virtual CDKW_V3d GetPosition();
+    virtual void SetPosition(CDKW_V3d& position);
+    virtual CDKW_V3d GetOrientation();
+    virtual void SetOrientation(CDKW_V3d& orientation);
+    virtual f32 GetYOrientation();
+    virtual void SetYOrientation(f32 y);
+    virtual CDKW_Matrix GetMatrix();
+    virtual void UpdateContacts();
+    virtual void ResolveContact(); // FIXME: params
+    virtual void UpdateVolumes();
+    virtual void ParseCollisions(DkXmd::CChunkIterator iter);
+    virtual void ParseCollisionsBVolume(DkXmd::CChunkIterator iter); // FIXME: params
+    virtual void ParseCollisionsBVolumeOBox(DkXmd::CChunkIterator iter); // FIXME: params
+};
