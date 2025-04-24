ea7956838c780ad82ce9cbad25339d0cf08e73e7
tgsm <doodrabbit@hotmail.com>
2025-04-24 02:05:37 -0500

    engine: Start working on CXmdFile
    This has been sitting on my computer for a little while now, but I
    haven't made any meaningful progress on Parse(). I'll just put it in
    anyway.

---
+++ src/engine/CXmdFile.cpp
@@ -0,0 +1,17 @@
+#include "CXmdFile.h"
+#include <cstring>
+
+namespace DkXmd {
+
+CXmdFile::CXmdFile() {
+    m_unk4 = NULL;
+    m_unk14 = 0;
+}
+
+CXmdFile::~CXmdFile() {
+    if (m_unk4 != NULL) {
+        delete[] m_unk4;
+    }
+}
+
+}
---
+++ src/engine/CXmdFile.h
@@ -0,0 +1,22 @@
+#pragma once on
+
+#include "types.h"
+#include "CChunkIterator.h"
+
+namespace DkXmd {
+
+class CXmdFile {
+private:
+    u32 m_unk0; // unknown return type
+    u32* m_unk4; // unknown return type
+    CChunkIterator m_chunk_iterator;
+    u32 m_unk14; // unknown return type
+
+public:
+    CXmdFile();
+    ~CXmdFile();
+
+    u32 Parse(void* unk);
+};
+
+}
